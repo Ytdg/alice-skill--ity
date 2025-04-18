224bc5601af493dc082ecc9c215d475771044d06
Nikita Bochkov <nikiljgryt@gmail.com>
2025-04-18 11:06:23 +0300

    init

---
+++ src/main/java/com/hackaton/alicecity/AliceCityApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.alicecity;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceCityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hackaton/alicecity/AliceCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.alicecity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
